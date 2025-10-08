d43ad299fff80a408a639f62425d7bf442fe0214
Adrián <adrian.rodriguez.roqueiro@gmail.com>
2025-10-08 12:59:40 +0200

    Crea pero no borra y rompe

--- src/Ej1/mainEj1.java
+++ src/Ej1/mainEj1.java
@@ -6,13 +6,14 @@ import java.util.Scanner;
 
 public class mainEj1 {
     public static void main(String[] args) throws SQLException {
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String user = "postgres";
         String password = "abc123.";
         String url = "jdbc:postgresql://localhost:5432/";
         String databaseNombre = "listaLibros";
 
         Connection connection = DriverManager.getConnection(url, user, password);
+
         try {
             Statement statement = connection.createStatement();
             statement.executeUpdate("drop database " + databaseNombre);
@@ -24,48 +25,52 @@ public class mainEj1 {
             Statement statement = connection.createStatement();
             statement.executeUpdate("create database " + databaseNombre + " ;");
             System.out.println("exito");
+
+            connection.close();
+
         } catch (SQLException e) {
             System.out.println("no se creo las BD de forma correcta");
         }
+        Connection connection2 = DriverManager.getConnection("jdbc:postgresql://localhost:5432/listaLibros", user, password);
         try {
-            Statement statement1= connection.createStatement();
+            Statement statement1 = connection2.createStatement();
             statement1.executeUpdate("CREATE SCHEMA objetos");
             statement1.executeUpdate("CREATE TYPE objetos.tipo_autor AS (nombre_autor varchar(255), fechaNacimiento varchar(100))");
             System.out.println("creada la tabla autor");
-            Statement statement2= connection.createStatement();
+            Statement statement2 = connection2.createStatement();
             statement2.executeUpdate("CREATE TABLE objetos.libros (id serial PRIMARY KEY, titulo VARCHAR, autor objetos.tipo_autor, año_publicacion integer)");
             System.out.println("Creados los libros");
-            connection.close();
+            connection2.close();
         } catch (SQLException e) {
             System.out.println("Los esquemas ya estaban creados");
         }
-        int opcion=1000;
+        int opcion = 1000;
         while (opcion != 0) {
             System.out.println("1 insertar");
             System.out.println("2 listar");
             System.out.println("3 actualizar");
             System.out.println("4 eliminar");
             System.out.println("0 salir");
-            opcion= scanner.nextInt();
+            opcion = scanner.nextInt();
             scanner.nextLine();
-            switch (opcion){
+            switch (opcion) {
                 case 1:
                     System.out.println("Nombre del autor");
-                    String nombre= scanner.nextLine();
+                    String nombre = scanner.nextLine();
                     System.out.println("fecha del autor");
-                    int fecha= scanner.nextInt();
+                    int fecha = scanner.nextInt();
                     scanner.nextLine();
-                    Autor autor=new Autor(nombre, fecha);
+                    Autor autor = new Autor(nombre, fecha);
                     System.out.println("titulo");
-                    String titulo= scanner.nextLine();
+                    String titulo = scanner.nextLine();
                     System.out.println("año");
-                    int año=scanner.nextInt();
+                    int año = scanner.nextInt();
                     try {
-                        PreparedStatement preparedStatement= connection.prepareStatement("INSERT INTO objetos.libros (titulo, autor, año_publicacion) VALUES(?, ROW(?, ?), ?)");
-                        preparedStatement.setString(1,titulo);
-                        preparedStatement.setString(2,autor.getNombre());
-                        preparedStatement.setInt(3,autor.getFecha());
-                        preparedStatement.setInt(4,año);
+                        PreparedStatement preparedStatement = connection2.prepareStatement("INSERT INTO objetos.libros (titulo, autor, año_publicacion) VALUES(?, ROW(?, ?), ?)");
+                        preparedStatement.setString(1, titulo);
+                        preparedStatement.setString(2, autor.getNombre());
+                        preparedStatement.setInt(3, autor.getFecha());
+                        preparedStatement.setInt(4, año);
                         preparedStatement.executeUpdate();
                         System.out.println("insertado");
                     } catch (SQLException e) {
@@ -74,16 +79,14 @@ public class mainEj1 {
                     break;
                 case 2:
                     String sql = """
-                        SELECT titulo, 
-                        (autor).nombre_autor AS nombre_autor,
-                        (autor).fechaNacimiento AS fechaNacimiento,
-                        año_publicacion 
-                        FROM objetos.libros
-                        """;
-
-                    PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                            SELECT titulo, 
+                            (autor).nombre_autor AS nombre_autor,
+                            (autor).fechaNacimiento AS fechaNacimiento,
+                            año_publicacion 
+                            FROM objetos.libros
+                            """;
+                    PreparedStatement preparedStatement = connection2.prepareStatement(sql);
                     ResultSet resultSet = preparedStatement.executeQuery();
-
                     while (resultSet.next()) {
                         String titulo1 = resultSet.getString("titulo");
                         String nombreAutor = resultSet.getString("nombre_autor");
@@ -95,7 +98,6 @@ public class mainEj1 {
                                 " | Nacimiento: " + fechaNacimiento +
                                 " | Año publicación: " + añoPublicacion);
                     }
-
                     break;
                 case 3:
                     break;
